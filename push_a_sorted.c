c90b8a72e05f206d768f3d3e560569c6da8eed05
Roberto <you@example.com>
2023-09-13 14:48:35 +0200

    norminette done, all finish

--- push_a_sorted.c
+++ push_a_sorted.c
@@ -6,11 +6,41 @@
 /*   By: rpliego <rpliego@student.42barcelo>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 03:13:31 by rpliego           #+#    #+#             */
-/*   Updated: 2023/09/13 04:40:48 by rpliego          ###   ########.fr       */
+/*   Updated: 2023/09/13 14:46:46 by rpliego          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	check_previous_rb(t_list *list, int aux)
+{
+	while (list -> stackb[0] < list -> lenb)
+	{
+		if (list -> stackb[0] == list -> lenb - 1 && aux != 1)
+		{
+			pa(list);
+			aux = 1;
+		}
+		else
+			rb(list);
+	}
+	return (aux);
+}
+
+int	check_previous_rrb(t_list *list, int aux)
+{
+	while (list -> stackb[0] < list -> lenb)
+	{
+		if (list -> stackb[0] == list -> lenb - 1 && aux != 1)
+		{
+			pa(list);
+			aux = 1;
+		}
+		else
+			rrb(list);
+	}
+	return (aux);
+}
 
 void	push_a_sorted(t_list *list)
 {
@@ -24,35 +54,11 @@ void	push_a_sorted(t_list *list)
 		while (list -> stackb[i] != list -> lenb)
 			i++;
 		if (i <= list -> lenb / 2)
-		{
-			while (list -> stackb[0] < list -> lenb)
-			{
-				if (list -> stackb[0] == list -> lenb - 1 && aux != 1)
-				{
-					pa(list);
-					aux = 1;
-				}
-				else
-					rb(list);
-			}
-		}
-		else
-		{
-			while (list -> stackb[0] < list -> lenb)
-			{
-				if (list -> stackb[0] == list -> lenb - 1 && aux != 1)
-				{
-					pa(list);
-					aux = 1;
-				}
-				else
-					rrb(list);
-			}
-		}
+			aux = check_previous_rb(list, aux);
+		else if (i > list -> lenb / 2)
+			aux = check_previous_rrb(list, aux);
 		pa(list);
 		if (aux == 1)
-		{
 			sa(list);
-		}
 	}
 }
